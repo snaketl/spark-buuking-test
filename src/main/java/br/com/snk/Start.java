623a060c9875f9469ef265b86f0fb8effd613e91
Luiz Henrique Feltes <snaketl@gmail.com>
2016-04-05 20:16:51 -0300

    Adjusted code style issues from Codacy

--- src/main/java/br/com/snk/Start.java
+++ src/main/java/br/com/snk/Start.java
@@ -3,7 +3,7 @@ package br.com.snk;
 public class Start {
     
     public static void main(String[] args) {
-    
+        // This constructor is intentionally empty, the application will mount the routes here
     }
     
 }
