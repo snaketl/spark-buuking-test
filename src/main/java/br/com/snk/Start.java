e693c6801de5a5861919df0cba30a761d4f6f159
Luiz Henrique Feltes <snaketl@gmail.com>
2016-04-06 19:46:08 -0300

    Added JCommander lib and config some command line parameters

--- src/main/java/br/com/snk/Start.java
+++ src/main/java/br/com/snk/Start.java
@@ -1,9 +1,46 @@
 package br.com.snk;
 
+import com.beust.jcommander.JCommander;
+import static spark.Spark.*;
+
+/**
+ * Start the application
+ * 
+ * @author SnK
+ */
 public class Start {
     
+    /**
+     * Start the application
+     * 
+     * @param args 
+     */
     public static void main(String[] args) {
-        // This constructor is intentionally empty, the application will mount the routes here
+        StartParameters parameters = loadParametersFromCommandLine(args);
+        configServer(parameters);
+    }
+    
+    /**
+     * Configure server
+     * 
+     * @param parameters Parameters
+     */
+    private static void configServer(StartParameters parameters) {
+        port(parameters.getPort());                
+        
+        
+    }
+    
+    /**
+     * Load parameters from command line
+     * 
+     * @param args
+     * @return StartParameters
+     */
+    private static StartParameters loadParametersFromCommandLine(String[] args) {
+        StartParameters parameters = new StartParameters();
+        JCommander jCommander = new JCommander(parameters, args);
+        return parameters;        
     }
     
 }
---
+++ src/main/java/br/com/snk/StartParameters.java
@@ -0,0 +1,24 @@
+package br.com.snk;
+
+import com.beust.jcommander.Parameter;
+
+/**
+ * Parameters
+ *
+ * @author SnK
+ */
+public class StartParameters {
+    
+    @Parameter(names = {"--port", "-p"}, description = "HTTP port number" )
+    private int port = 8080;
+
+    /**
+     * Get HTTP port
+     * 
+     * @return HTTP port 
+     */
+    public int getPort() {
+        return port;
+    }
+    
+}
